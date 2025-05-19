677700151e8d2c395df485d7b594c8c983f6fcf5
Jose05791 <josespinel@uniboyaca.edu.co>
2025-05-19 11:10:43 -0500

    RegisterController

---
+++ views/actions/registerCategories.php
@@ -0,0 +1,39 @@
+<?php
+include '../../models/drivers/conexDB.php';
+include '../../models/entities/model.php';
+include '../../models/entities/categories.php';
+include '../../controller/Categoriescontroller.php';
+
+use app\controller\CategoriesController;
+$controller = new CategoriesController();
+
+
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    header('location: ../dishes.php');
+}
+$res = empty($_POST['idDish'])
+    ? $controller->saveNewCategorie($_POST)
+    : $controller->updateCategorie($_POST);
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado operación</title>
+</head>
+
+<body>
+    <h1>Resultado de la operación</h1>
+    <?php
+    if ($res == 'yes') {
+        echo '<p>Datos guardados</p>';
+    } else {
+        echo  '<p>No se pudo guardar los datos</p>';
+    }
+    ?>
+    <br>
+    <a href="../formCategories.php">Volver</a>
+</body>
+
+
+
+?>
